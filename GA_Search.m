43bf2fd36f11f7de7b682127d82ab3d8c7bc019c
danielgerlei <daniel.gerlei@gmail.com>
2016-10-13 14:13:09 +0100

    Add files via upload

--- GA_Search.m
+++ GA_Search.m
@@ -1,42 +1,26 @@
 % Genetic Algorithms in Matlab (minimising fitness)
-function[m,c]=GA_Search(numberOfRovers)
+function[chrom,coord,F]=GA_Search(numberOfRovers,chrom,coord,F)
 % Initial Simulation Data
+%Initial Data
 global targetX;
 global targetY;
 global targetR;
-
-i=1;                            % counter
-
-% Initial Data
-
-genNum=10;                      % number of generations
+genNum=5;                       % number of generations
 popSize=numberOfRovers;         % size of population
-parNum=2;                       % number of parents (number of dimensions)
-chrLen=10;                      % length of chromosomes
-childNum=popSize-2;             % number of children
+global parNum;                  % number of parents (number of dimensions)
+global chrLen;                  % length of chromosomes
+childNum=round(0.8*popSize);    % number of children
 popSize2=popSize-childNum;      % number of elite agents
 num=(popSize2+1):popSize;       % vector to point at the rest of the population
 mutRate=5;                      % mutation rate
 bot = 1;                        % floor to start loops from
 
-
-% Creating Initial Chromosomes 
-[chrom,coord]=createFirstGen(popSize,chrLen,parNum);
-
-[F]=evaluate(coord,popSize,targetX,targetY,targetR);    % evaluate the first generation
-
-[chrom,coord,F]=sort_swap(chrom,coord,F,popSize);		% sort outcomes according to F score for reproduction (elitism)
-
-% create and evaluate following generations of chromosomes
+% create and evaluate next generation of chromosomes
 for gen=bot:genNum,
 	kinder=crossover2p(popSize,childNum,chrom,chrLen);              % 2 point crossover to create children
 	kinder=mutate(mutRate,childNum,chrLen,kinder);                  % mutation to ensure genetic diversity
 	kidCoord=decode(kinder,childNum,parNum);                        % decoding children
     [coord,chrom]=assembleGen(coord,chrom,num,kidCoord,kinder);     % assemble new generation, keeping the elite
     [F]=evaluate(coord,popSize,targetX,targetY,targetR);            % evaluate new generation
-    [chrom,coord,F]=sort_swap(chrom,coord,F,popSize);               % sort new generation
-   
+    [chrom,coord,F]=sort_swap(chrom,coord,F,popSize);               % sort new generation 
 end
-m=coord(1,1)		% display best results
-c=coord(2,1)
-a=F(1)
--- Move_rover.m
+++ Move_rover.m
@@ -3,40 +3,58 @@ clear
 global targetX;
 global targetY;
 global targetR;
-numberOfRovers = 8;
-roverX = 100;                   % lead rover start position
-roverY = 100;
-Xvel = 0;                       % initial velocities
-Yvel = 0;
-maxSpeed = 0.2;
-timelimit = 1000;
+global chrLen;
+global parNum;
+
+chrLen = 10;
+parNum = 2;
+roverNum = 10;                  % number of rovers
+roverX(1) = 100;                % lead rover start position
+roverY(1) = 100;
+for ind = 2:roverNum
+    roverX(ind)=roverX(ind-1);
+    roverY(ind)=roverY(ind-1)+0.5;
+end
+Xvel(1:roverNum) = 0;           % initial velocities set to zero
+Yvel(1:roverNum) = 0;
+maxSpeed = 0.2;                 % speed limiter on rovers
 
 targetX=[35,72,140];            % target positions        
 targetY=[67,58,125];                      
-targetR=[5,5,5];
-pathX(1)=roverX;
-pathY(1)=roverY;
-pointer = 2;
+targetR=[10,10,10];
+pathX(:,1)=roverX(:);
+pathY(:,1)=roverY(:);
+pointer(1:roverNum) = 2;
 i=2;
-for ind=1:100                    % run for 10 waypoints
-    [nextX,nextY]=GA_Search(numberOfRovers);
-    waypointX(ind) = nextX;
-    waypointY(ind) = nextY;
-    axis([0 200 0 200])
-    dist = ((nextX-roverX)^2 + (nextY-roverY)^2)^(1/2);
-    timer = 0;                  % reset timer
-    [Xpos,Ypos]=getPath(Xvel,Yvel,nextX,nextY,roverX,roverY,maxSpeed);
-    pathEnd = size(Xpos,2)+pointer-1;
-    pathX(pointer:pathEnd)=Xpos;
-    pathY(pointer:pathEnd)=Ypos;
-    roverX = pathX(pathEnd);
-    roverY = pathY(pathEnd);
-    pointer = pathEnd;
+
+% Creating Initial Chromosomes 
+[chrom,coord]=createFirstGen(roverNum,chrLen,parNum);
+[F]=evaluate(coord,roverNum,targetX,targetY,targetR);    % evaluate the first generation
+[chrom,coord,F]=sort_swap(chrom,coord,F,roverNum);		% sort outcomes according to F score for reproduction (elitism)
+
+for ind=1:50                 % run for 5 waypoints
+    [chrom,coord,F]=GA_Search(roverNum,chrom,coord,F);
+    waypoints = coord;        % set up temporary matrix to store waypoints
+    nextX(:) = waypoints(1,:);
+    nextY(:) = waypoints(2,:);  
+    waypointX(ind,:) = nextX(:);
+    waypointY(ind,:) = nextY(:);
+    for c=1:roverNum
+        [Xpos,Ypos]=getPath(Xvel(c),Yvel(c),nextX(c),nextY(c),roverX(c),roverY(c),maxSpeed);
+        pathEnd(c) = size(Xpos,2)+pointer(c)-1;
+        pathX(c,pointer(c):pathEnd(c))=Xpos;
+        pathY(c,pointer(c):pathEnd(c))=Ypos;
+        roverX(c) = pathX(c,pathEnd(c));
+        roverY(c) = pathY(c,pathEnd(c));
+        pointer(c) = pathEnd(c);
+    end    
 end
  figure (1)
  clf
- plot(pathX,pathY,'g') 
- hold on
+ for i=1:roverNum
+     plot(pathX(i,:),pathY(i,:)) 
+     hold on
+ end
  plot(waypointX,waypointY,'bx') 
  plot(targetX,targetY,'ro')
  axis([0 200 0 200])
